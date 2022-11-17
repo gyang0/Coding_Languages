5d66a1ac0dad75347228677f42fc3c37aeb8bf5f
gyang0 <107653576+gyang0@users.noreply.github.com>
2022-11-16 18:38:08 -0800

    Update stuff.js

--- stuff.js
+++ stuff.js
@@ -1,7 +1,15 @@
 // Page switching
 function goTo(pageID){
-  
+    
+    // Hide all the pages at first.
+    var classes = document.getElementsByClassName("pages");
+    for(var i = 0; i < classes.length; i++){
+        classes[i].style.display = "none";
+    }
+        
+    // Show the intended page.
+    classes[pageID].style.display = "block";
 }
 
-// Navbar        
-    
+// Avoid displaying them all at once initially
+goTo(0); 
